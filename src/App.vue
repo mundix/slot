1f732ba12eb3e3133565f4181adf2e5994e1b31e
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-25 09:54:44 -0400

    creamos varios slots  para poner contenido por referencia de nombres

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,23 @@
       <!-- <mostrarFormulario titulo="Cambiar Contraseña"></mostrarFormulario> -->
         <!-- Quiero pasar el titulo de esta forma usando slot  -->
       <mostrarFormulario>
-        <h3>Cambiar Contrasenña</h3>
-        <div>
+        <div slot="titulo">
+          <h3>Cambiar Contrasenña</h3>
+        </div>
+        <div slot="elementos">
           <div class="form-group">
             <label for="correo">Correo Electrónico</label>
             <input type="email" class="form-control"
               id="correo" placeholder="Escribe tu correo electronico">
           </div>
-          <div class="form-group">
+          <!-- <div class="form-group">
             <label for="clave">Contraseña</label>
             <input type="password" class="form-control"
               id="clave" placeholder="Escribe tu Contraseña">
-          </div>
-          <button class="btn btn-primary">Iniciar Sesión</button>
+          </div> -->
+        </div>
+        <div slot="boton">
+          <button class="btn btn-primary">Cambiar Contrasenña</button>
         </div>
       </mostrarFormulario>
     </div>
@@ -36,6 +40,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  h3 {
+    color:red;
+  }
 </style>
--- src/mostrarFormulario.vue
+++ src/mostrarFormulario.vue
@@ -1,28 +1,18 @@
 <template>
     <form action="" class="form">
-      <!-- <h3>{{titulo}}</h3> -->
-      <!-- Slot me permite mostrar todo lo que esta dentro de la etiqueta mostrarFormulario -->
-      <!-- Algo asi como el yield de laravel  -->
-      <slot></slot>
-      <!-- <div class="form-group">
-        <label for="correo">Correo Electrónico</label>
-        <input type="email" class="form-control"
-          id="correo" placeholder="Escribe tu correo electronico">
-      </div>
-      <div class="form-group">
-        <label for="clave">Contraseña</label>
-        <input type="password" class="form-control"
-          id="clave" placeholder="Escribe tu Contraseña">
-      </div>
-      <button class="btn btn-primary">Iniciar Sesión</button> -->
+      <slot name="titulo"></slot>
+      <slot name="elementos"></slot>
+      <slot name="boton"></slot>
     </form>
 </template>
 <script>
   export default {
-    // props: ['titulo']
+
   }
 </script>
-<style>
-
+<style scoped>
+  h3 {
+    color:green;
+  }
 </style>
 
