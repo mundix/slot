f2780874bbe8fd7af5016677cab6949224acd7e9
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-25 08:42:57 -0400

    Creamos el formulario

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="container">
     <div class="jumbotron">
-
+      <mostrarFormulario></mostrarFormulario>
     </div>
   </div>
 </template>
 
 <script>
 // Vamos a crear un componente que tenga varios formularioo uno
+import MostrarFormulario from './mostrarFormulario';
 export default {
-  data () {
+  components: {
+    mostrarFormulario: MostrarFormulario
+  },
+  data(){
 
   }
 }
--- src/mostrarFormulario.vue
+++ src/mostrarFormulario.vue
@@ -1,5 +1,18 @@
 <template>
-
+    <form action="" class="form">
+      <h3>Iniciar Sessionó</h3>
+      <div class="form-group">
+        <label for="correo">Correo Electrónico</label>
+        <input type="email" class="form-control"
+          id="correo" placeholder="Escribe tu correo electronico">
+      </div>
+      <div class="form-group">
+        <label for="clave">Contraseña</label>
+        <input type="password" class="form-control"
+          id="clave" placeholder="Escribe tu Contraseña">
+      </div>
+      <button class="btn btn-primary">Iniciar Sesión</button>
+    </form>
 </template>
 <script>
 
